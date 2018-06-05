4b5c77ef9ee8c612a1463961367c2ee4249524e7
marcelo046 <marc3lo046@gmail.com>
2018-06-05 02:01:11 -0300

    adicionado matriculas

--- projeto4.c
+++ projeto4.c
@@ -1,6 +1,6 @@
-//Alunos: Fabiana Luiza Vasconcelos Pfeilsticker Ribas
-//        Joao Pedro Mota Jardim
-//        Marcelo Araujo dos Santos
+//Alunos: Fabiana Luiza Vasconcelos Pfeilsticker Ribas  16/0005736
+//        Joao Pedro Mota Jardim                        16/0031982
+//        Marcelo Araujo dos Santos                     16/0035481
 
 #include <stdio.h>
 #include <stdlib.h>
